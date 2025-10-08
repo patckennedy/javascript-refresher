8aaeeb926e4c2e1ab7ece69d83377562adaa75c6
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-10-08 17:43:26 -0500

    feat: add click handler (calculateButton) to read & parse billInput and tipPercentInput values

--- mini-projects/tip-calculator/script.js
+++ mini-projects/tip-calculator/script.js
@@ -1,15 +1,20 @@
 // console.log('Tip Calculator Ready1');
 
 function tipCalculator(bill, tipPrecent) {
- if (isNaN(bill) || isNaN(tipPrecent) || bill < 0 || tipPrecent < 0) return NaN;
- const tip = bill * (tipPrecent / 100);
- const total = bill + tip;
- return { tip, total };
+    if (isNaN(bill) || isNaN(tipPrecent) || bill < 0 || tipPrecent < 0)
+        return NaN;
+    const tip = bill * (tipPrecent / 100);
+    const total = bill + tip;
+    return { tip, total };
 }
 
 // UI
 const billInput = document.getElementById('bill');
 const tipPercentInput = document.getElementById('tipPercent');
-const calculateButton = document.getElementById("calculate");
-const result = document.getElementById("result");
+const calculateButton = document.getElementById('calculate');
+const result = document.getElementById('result');
 
+calculateButton.addEventListener('click', function () {
+    const bill = Number(billInput.value);
+    const tipPercent = Number(tipPercentInput.value);
+});
