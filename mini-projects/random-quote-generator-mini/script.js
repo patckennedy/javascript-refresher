3cc244c8eb523f9c24c54779e9fec35d4319794e
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-11-17 20:01:44 -0600

    feat: add simple list of quotes

---
+++ mini-projects/random-quote-generator-mini/script.js
@@ -0,0 +1,27 @@
+// Simple list of quotes
+const quotes = [
+    {
+        text: 'The best time to start was yesterday. The next best time is now.',
+        author: 'Unknown',
+    },
+    {
+        text: 'It always seems impossible until it’s done.',
+        author: 'Nelson Mandela',
+    },
+    {
+        text: 'Do what you can, with what you have, where you are.',
+        author: 'Theodore Roosevelt',
+    },
+    {
+        text: 'Slow progress is still progress.',
+        author: 'Unknown',
+    },
+    {
+        text: 'Success is the sum of small efforts repeated day in and day out.',
+        author: 'Robert Collier',
+    },
+    {
+        text: 'You don’t have to be perfect to be amazing.',
+        author: 'Unknown',
+    },
+];
